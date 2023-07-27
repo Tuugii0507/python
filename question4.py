eff7974fe4f7bc6693d0b8ad131afabf8fc8848e
Tuugii <tuugii.1980.b@gmail.com>
2023-07-27 12:47:58 +0800

    fuck

--- question4.py
+++ question4.py
@@ -5,19 +5,15 @@
 
 
 def my_function(arr1, arr2):
+
     result = []
     for x in arr1:
         for y in arr2: 
-            for xa in x: 
-                last_words_index = arr1[len(arr1) - 1]
-                # print(arr1.index(last_words_index), len(arr1), xa)
-                if last_words_index == arr1[len(arr1) - 1]:
-                    print(last_words_index, arr1[len(arr1) - 1])
+            for xa in x:
+                if (arr1.index(x) == len(arr1) - 1 ):
+                    return [*set(result)]
+                elif (xa == y):
+                    result.append(x) 
                     
-                # if(xa == y):
-                #     result.append(x)
-                #     # print([*set(result)])
-
-
-a = my_function(["Giorgio", "Irene", "Antonio", "Carla", "Arthur", "Steve"], ["o", "e"])
-# print(a) #["Giorgio", "Irene", "Antonio"]
+a = my_function(["Giorgio", "Irene", "Antonio", "Carla",], ["o", "e"])
+print(a) #["Giorgio", "Irene", "Antonio"]
